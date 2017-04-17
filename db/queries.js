2f7e9897757b44f7039d0d066ab0a11b7c69bcbb
joellegg <joellegg@bellsouth.net>
2017-04-17 11:54:00 -0500

    set up files and test that fails

--- db/queries.js
+++ db/queries.js
@@ -1,6 +1,6 @@
 // Added this file after writing the first failing test.
-// Here, we made a reference to our database via the Knex config file, 
-// added a helper function for simplifying each individual query, 
+// Here, we made a reference to our database via the Knex config file,
+// added a helper function for simplifying each individual query,
 // and finally queried the database to get ALL shows.
 
 const { knex, bookshelf } = require('./database');
@@ -18,12 +18,12 @@ const addNewFave = (fave, user) => {
 const getMyFaves = (id) => {
   console.log("userId", id);
   // return Favorite.forge().query({where: {user_id}}).fetchAll()
-  // 
+  //
   // we can use “forge”, which is a simple helper function to instantiate a new Model without needing “new”.
-  // we want to be able to get a specific user by its id. For this, we chain another Bookshelf method 
-  // called “query”. This method is used to tap into the underlying Knex query builder instance for the 
-  // current model. We can now use the Knex method “where”, which is a wrapper for the standard SQL 
-  // where statement. 
+  // we want to be able to get a specific user by its id. For this, we chain another Bookshelf method
+  // called “query”. This method is used to tap into the underlying Knex query builder instance for the
+  // current model. We can now use the Knex method “where”, which is a wrapper for the standard SQL
+  // where statement.
   let user = User.forge().query({where: {'id': id}})
   return user.fetch({withRelated: ['favoriteShows'], require: true})
   .then( (user) => {
@@ -32,10 +32,10 @@ const getMyFaves = (id) => {
     // If this was an API, we would do this here instead:
     // res.status(200).json(user.relations.favoriteShows);
   })
-   // If there are any errors in our Series query, it will be sent to the “catch” method. 
+   // If there are any errors in our Series query, it will be sent to the “catch” method.
    // For now, we are just going to return the error and have the function that calls 'getMyFaves`
-   // send the “next” callback to the catch method and 
-   // let our error handling middleware take care of the error. 
+   // send the “next” callback to the catch method and
+   // let our error handling middleware take care of the error.
   .catch( (error) => {
     // console.log("error??", error);
     throw error;
