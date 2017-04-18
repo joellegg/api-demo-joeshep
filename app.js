66497d254d1ae8051e3e7df55c0f3b5872898a94
joellegg <joellegg@bellsouth.net>
2017-04-18 16:00:37 -0500

    uncommented server listener

--- app.js
+++ app.js
@@ -44,8 +44,8 @@ app.use( (err, req, res, next) => {
 })
 
 const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log(`Listening on port ${port} in this super keen env: ${process.env.NODE_ENV}`);
-// });
+app.listen(port, () => {
+  console.log(`Listening on port ${port} in this super keen env: ${process.env.NODE_ENV}`);
+});
 
 module.exports = app;
