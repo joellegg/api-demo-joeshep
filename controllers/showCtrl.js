f087f82e7c18bc2682b1ee059fdcdb5c5d7d837b
joellegg <joellegg@bellsouth.net>
2017-04-18 15:29:42 -0500

    added functions and test for adding and deleting a show

--- controllers/showCtrl.js
+++ controllers/showCtrl.js
@@ -13,28 +13,44 @@ module.exports.getShows = (req, res, next) => {
   })
 }
 
-module.exports.getShow = (req, res, next) => {
-  Show.forge({id: req.params.id})
-  .fetch()
-  .then((show) => {
+module.exports.getShow = ({params: {id}}, res, next) => {
+  Show.getSingleShow(id)
+  .then( (show) => {
     res.status(200).json(show)
   })
-}
-
-module.exports.getShowFaves = ({query: {showId}}, res, next) => {
-  console.log('The query string', showId)
-  Show.forge({id: showId})
-  .fetch({withRelated: ['upvotes'], require: true})
-  .then((faves) => {
-    res.status(200).json(faves)
-  })
-  .catch(err => next(err))
-}
+  .catch( (error) => {
+    next(error);
+  });
+};
 
 module.exports.addShow = ({body}, res, next) => {
   Show.forge(body)
   .save()
-  .then(() => {
-    res.status(201).json({'msg': 'Nice POST'})
+  .then( () => res.status(201).json({"msg": "Nice POST, brah"}))
+  .catch( (error) => {
+    next(err);
+  });
+};
+
+module.exports.deleteShow = ({params: {id}}, res, next) => {
+  Show.forge({id})
+  .destroy()
+  .then( (show) => {
+    res.status(202).json(show);
   })
-}
+  .catch( (err) => {
+    next(err);
+  });
+};
+
+module.exports.getShowFaves = ({query: {showId}}, res, next) => {
+  console.log("The query string", showId);
+  Show.forge({id: showId})
+  .fetch({withRelated: ['upvotes'], require: true})
+  .then( (faves) => {
+    res.status(200).json(faves)
+  })
+  .catch( (err) => {
+    next(err);
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@
 const { Router } = require('express')
 const router = Router()
 
-const { getShows, getShow, getShowFaves, addShow } = require('../controllers/showCtrl')
+const { getShows, getShow, getShowFaves, addShow, deleteShow } = require('../controllers/showCtrl')
 
-router.get('/shows', getShows)
-router.get('/shows/:id', getShow)
-router.get('/shows/favorites', getShowFaves)
-// router.post('/shows/...')
+router.get('/shows', getShows);
+router.post('/shows/new', addShow)
+router.get('/shows/favorites', getShowFaves);
+router.get('/shows/:id', getShow);
+router.delete('/shows/:id', deleteShow);
 
-module.exports = router
+module.exports = router;
